adfe2067bd99b1f1896e0575942cf7e1ee28a858
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-27 18:58:27 +0530

    Update __init__.py

--- MukeshRobot/database/__init__.py
+++ MukeshRobot/database/__init__.py
@@ -1,1 +1,12 @@
+#MIT License
+#Copyright (c) 2023, ©NovaNetworks
+
+from async_pymongo import AsyncClient
+
+from MukeshRobot import MONGO_DB_URI
+
+DBNAME = "MukeshRobot"
+
+mongo = AsyncClient(MONGO_DB_URI)
+dbname = mongo[DBNAME]
 
