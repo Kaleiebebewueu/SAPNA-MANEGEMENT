f65cef6e9e0a3d83bf31c165d58e7c5cbdbbaa69
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-07-08 13:51:35 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,22 +4,22 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6762113050 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = "28795512" # integer value, dont use ""
+    API_HASH = "c17e4eb6d994c9892b8a8b6bfea4042a"
+    TOKEN = "6827959192:AAF2IvJce014rPQ8Y6JNSo5tRD82EnX3UUQ"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 7115307617 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "MASTIWITHFRIENDSX"  # Your own group for support, do not add the @
-    START_IMG = ""
-    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    SUPPORT_CHAT = "ALL_SANATANI_BOT"  # Your own group for support, do not add the @
+    START_IMG = "https://telegra.ph//file/00c5804219a122d0c959b.jpg"
+    EVENT_LOGS = (-1001970127211)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    MONGO_DB_URI= "mongodb+srv://SachinSanatani:SACHINxSANATANI@sanatani.bnmsfbd.mongodb.net/?retryWrites=true&w=majority&appName=Sanatani"
     # RECOMMENDED
-BOT_USERNAME = getenv("BOT_USERNAME" , "PoliceBot")
+    BOT_USERNAME = getenv("BOT_USERNAME" , "SANATANI_TECH_BOT")
     DATABASE_URL = getenv("DATABASE_URL", " postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc")
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "HD7RYGCTES1V"
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
